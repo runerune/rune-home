2150ad7acd957ef6b10a718016ec53a55243397e
runerune <guildbot@bezkr.es>
2020-06-27 13:24:30 +0200

    remove default title test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
 		const app = fixture.componentInstance;
 		expect(app.title).toEqual('rune-home');
 	});
-
-	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.nativeElement;
-		expect(compiled.querySelector('.content span').textContent).toContain('rune-home app is running!');
-	});
 });
