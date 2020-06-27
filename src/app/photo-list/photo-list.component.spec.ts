99b69e07f4683c1d4492262c76933994da63e4b0
runerune <guildbot@bezkr.es>
2020-06-27 19:44:58 +0200

    add spec for photo list

--- src/app/photo-list/photo-list.component.spec.ts
+++ src/app/photo-list/photo-list.component.spec.ts
@@ -22,4 +22,35 @@ describe('PhotoListComponent', () => {
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('should init module', () => {
+		expect(component.photos).toEqual(jasmine.any(Array));
+		expect(component.selected).toBe(0);
+		expect(component.load).toEqual(jasmine.any(Function));
+	});
+		
+	it('should load photos module', async () => {
+		spyOn(component, 'load').and.returnValue([{ foo: 'bar' }]);
+		await component.load();
+		
+		expect(component.photos.length).toBeGreaterThan(0);
+		expect(component.photos[0]).toBe({ foo: 'bar' });
+	});
+
+	it('should select a photo', async () => {
+		component.photos = ['a','s','d','f'];
+		component.select(42);
+		
+		expect(component.selected).toBe(2);
+	});
+
+	it('should not select a photo that doesnt exist', async () => {
+		component.photos = ['a','s','d','f'];
+		component.select(99);
+		
+		expect(component.selected).toBe(4);
+	});
+
+	
+	
 });
